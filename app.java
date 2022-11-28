dd60149893f72b6c662fcfd4bb5e65b2503255df
Akeem <akeem.adedipe1@egmail.com>
2022-11-28 06:45:10 +0000

    recovered and added missing text

--- app.java
+++ app.java
@@ -1,2 +1,5 @@
-added more database content to demostrate addition to file
-i deleted previous file and need to figure it out
+#This is a code for a web application
+#Please do not hardcode
+#Make use of dynamic variables
+#added more database content to demostrate addition to file
+#i deleted previous file and need to figure it out
