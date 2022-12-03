ae23e18e9f5bf36f307a68e149ebec9885e8407e
Akeem <akeem.adedipe1@egmail.com>
2022-12-03 01:07:18 +0000

    commit

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-just testing testing adding additional file to commit and push to repo
-hhkjkjkjj
-hhhhhkdjkjj
-jjggjfdhbnmbmn
-vsdhfhfjj
-modified this file in remote and will pull back to local repo
--- devcode.app
+++
@@ -1,3 +0,0 @@
-this is a code to test git barnching for class30 group 14
-
-just an example to play with
